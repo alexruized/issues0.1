52183c479f76dd5072b607ba7261ef6e64743b86
alexruized <practicaentornosgit@gmail.com>
2024-05-21 17:52:05 +0200

    Add files via upload

---
+++ ej04_pares.py
@@ -0,0 +1,19 @@
+# Escribir un programa que imprima todos los números pares entre dos números que se le pidan al usuario.
+
+# Alejandro Ruiz
+
+lim_par_1 = int(input('dime el primer limite : '))
+lim_par_2 = int(input('Dime el segundo limite : '))
+
+
+if lim_par_1 // 2 != 0 and lim_par_1 != 2:
+    lim_par_1 += 1
+
+
+if lim_par_2 < lim_par_1:
+    aux = lim_par_1
+    lim_par_1 = lim_par_2
+    lim_par_2 = aux
+
+for n in range(lim_par_1, lim_par_2 + 1, 2):
+    print(n)
